b7f6aa2c986daf32e797c036c853997e4df9bd32
aishwaryashelar3097 <imaishwaryashelar@gmail.com>
2021-04-23 15:27:27 +0530

    pushed UC3

--- src/UserRegistration.js
+++ src/UserRegistration.js
@@ -30,7 +30,19 @@ if (result1 == true) {
     console.log(" your input is invalid");
 
 }
+const EmailRegex = new RegExp('^[a-zA-Z0-9]+([_+-.]{0,1})([@]{1})[a-z0-1]+.[a-z]+(.?)([a-z]{2}?)$');
+var Email = read.question('Enter your email')
+var result2 = EmailRegex.test(Email)
 
+console.log(` ${result2},`);
+
+if (result2 == true) {
+    console.log("your input is valid");
+
+} else {
+    console.log(" your input is invalid");
+
+}
 
 
 
